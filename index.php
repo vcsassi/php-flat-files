497ae8cad487b4dfcc00b9f107365a1067242ce9
vcsassi <vcsassi@gmail.com>
2015-08-04 11:21:31 -0500

    PHP flat files mini-assigment
    add text

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-
+<?php
+$file = fopen("einstein.txt", "r") or die("unable to open file!");
+$file_data = fread($file, fielsize("einstein.txt"));
+fclose($file);
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Flat File Mini Assignment</title>
     </head>
     <body>
-        <h1>Display Your text flat file below</h1>
+        <h1>Albert Einstein Quotes</h1>
+        <form action="new_line_adder.php" method="POST">
+        	<fieldset>
+        	<legend>Add this line to the end</legend>
+        	<input type="text" name="It is the supreme art of the teacher to awaken joy in creative expression and knowledge."/>
+        	<input type="submit">
+        	</fieldset>
+        </form>
+        <pre><?php echo $file_data; ?></pre>
     </body>
 </html>
